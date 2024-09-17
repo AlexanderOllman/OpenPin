8fa71a0a9fd1cbf8173bd84e3da2763590db2375
AlexanderOllman <healfheonix@hotmail.com>
2024-09-16 21:27:20 -0700

    message test

--- pebble_test.py
+++ pebble_test.py
@@ -1,7 +1,12 @@
 from libpebble2.communication import PebbleConnection
 from libpebble2.communication.transports.serial import SerialTransport
+
+from libpebble2.services import *
+from libpebble2.services.notifications import Notifications
+
 pebble = PebbleConnection(SerialTransport("/dev/rfcomm0"))
 pebble.connect()
 pebble.run_async()
 
-print(pebble.watch_info.serial)
+n = Notifications(pebble, None)
+n.send_notification("Subject", "Message", "From")
